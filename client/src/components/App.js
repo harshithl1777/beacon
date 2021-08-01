c48113445e28f324b254ffd20349c1b7d24a9cc5
Harshith Latchupatula <harshithl1777@gmail.com>
2021-08-01 18:01:23 -0400

    rename wgsi release command and test heroku deploy

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>Welcome to Beacon. We're still reopening up. Check back later please!</div>
+        <div>Welcome to Beacon. We're still reopening up. Check back later please.</div>
     );
 }
 
