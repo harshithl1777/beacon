190cd92ae333f644dfb48cf0e0449c5c3e56de96
Harshith Latchupatula <harshithl1777@gmail.com>
2021-08-12 08:54:10 -0400

    add base button component

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,7 +1,27 @@
 import styles from 'components/Button/Button.module.scss';
+import PropTypes from 'prop-types';
 
-const Button = () => {
-	return <div className={styles.test}>Hello Test</div>;
+const Button = (props) => {
+	const { text, type, disabled, ...rest } = props;
+
+	return (
+		<button {...rest} disabled={disabled}>
+			{text}
+		</button>
+	);
+};
+
+Button.propTypes = {
+	text: PropTypes.string.isRequired,
+	type: PropTypes.oneOf(['primary', 'secondary', 'blue', 'purple', 'danger']),
+	disabled: PropTypes.bool,
+	icon: PropTypes.string,
+};
+
+Button.defaultProps = {
+	type: 'primary',
+	disabled: false,
+	icon: null,
 };
 
 export default Button;
