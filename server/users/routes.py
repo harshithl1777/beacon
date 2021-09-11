bd3d53567e66f11fa6d3a2fbc2bf7abbd473ecc3
Harshith Latchupatula <harshithl1777@gmail.com>
2021-09-11 19:36:17 -0400

    add pyjwt and test git hook

--- server/users/routes.py
+++ server/users/routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 users_bp = Blueprint('users_bp', __name__)
 
-@users_bp.route('/test')
+@users_bp.route('/test', methods=['POST'])
 def getUsers():
-    return 'Yash Kapoor'
+    print(request.headers.get('Authorization'))
+    return {
+        "hello": "hello"
+    }
