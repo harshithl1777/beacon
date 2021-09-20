566fc6132f1509ea28b2f56f3921b2cca7216322
Harshith Latchupatula <harshithl1777@gmail.com>
2021-09-19 23:30:27 -0400

    fix python version error

--- server/users/models.py
+++ server/users/models.py
@@ -8,5 +8,4 @@ class User(Document):
     email = EmailField(required=True)
     password = StringField(max_length=200)
     auth_method = StringField(['EMAIL_PW', 'GOOGLE', 'FACEBOOK', 'GITHUB'])
-    address = Geo
     join_date = DateTimeField(default=datetime.utcnow(), required=True)
