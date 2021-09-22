d8d21ca63d45589ba9d9d857cefccc9f4e3ddb96
Harshith Latchupatula <harshithl1777@gmail.com>
2021-09-22 09:31:51 -0400

    test final git hook

--- server/utils/decorators/auth.py
+++ server/utils/decorators/auth.py
@@ -14,7 +14,7 @@ def require_access_token(method):
             access_token = request.headers.get('Authorization').split(' ')[1]
             print(access_token)
             if not check_jwt(access_token, 'ACCESS'):
-                return create_response('Invalid or expired access token', False, 401)
+                return create_response('Invalid access token', False, 401)
             else:
                 return method(*args, **kwargs)
     return check_access_token
