2b3c4e17b2e35ff9ab1c254fe97fa887d9a08b87
Harshith Latchupatula <harshithl1777@gmail.com>
2021-10-04 08:09:05 -0400

    add hashing and salting to create user route

--- server/users/models.py
+++ server/users/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
-from mongoengine.document import Document, EmbeddedDocument
-from mongoengine.fields import DateTimeField, EmailField, StringField, DictField
+from mongoengine.document import Document
+from mongoengine.fields import DateTimeField, EmailField, StringField
 
 
 class User(Document):
     meta = {'collection': 'users'}
     email = EmailField(required=True)
-    password = StringField(max_length=200)
-    # salt = StringField(max_length=200)
-    # auth_method = StringField(['EMAIL_PW', 'GOOGLE', 'FACEBOOK', 'GITHUB'])
-    # join_date = DateTimeField(default=datetime.utcnow(), required=True)
+    password = StringField(max_length=200, required=True)
+    salt = StringField(max_length=200, required=True)
+    auth_method = StringField(
+        choices=('EMAIL_PW', 'GOOGLE', 'FACEBOOK', 'GITHUB'), default='EMAIL_PW')
+    join_date = DateTimeField(default=datetime.utcnow())
--- server/users/routes.py
+++ server/users/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 import json
+import bcrypt
 
 from server.users.models import User
 from server.auth.decorators import require_access_token
@@ -8,8 +9,18 @@ from server.utils.helpers.routes import create_response
 users = Blueprint('users', __name__)
 
 
+@users.route('/', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    email, password = body.get('email'), body.get('password'),
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt).hex()
+    newUser = User(email=email, password=hashed_password, salt=salt.hex())
+    return create_response(payload=json.loads(newUser.to_json()), code=201)
+
+
 @users.route('/<string:user_id>', methods=['GET'])
-@require_access_token
+# @require_access_token
 def get_user_by_id(user_id: str):
-    matchingUser = json.loads(User.objects.get(id=user_id).to_json())
-    return create_response(matchingUser)
+    matchingUser = User.objects.get(id=user_id).to_json()
+    return create_response(json.loads(matchingUser))
