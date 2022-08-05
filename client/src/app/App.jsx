ca65dc6eb6f15615a95b205eab4c701334bafade
Harshith <harshithl1777@gmail.com>
2022-08-04 21:15:47 -0400

    finish dropdown component functionality

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -8,39 +8,40 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'app/global.scss';
 
 const App = () => (
-	<>
-		<Router>
-			<Routes>
-				<Route path='/' element={<Navigate to='/app/home' />} />
-				<Route
-					path='/auth/signup'
-					element={
-						<Gateway>
-							<SignupPage />
-						</Gateway>
-					}
-				/>
-				<Route
-					path='/auth/login'
-					element={
-						<Gateway>
-							<LoginPage />
-						</Gateway>
-					}
-				/>
-				<Route
-					path='/app/home'
-					element={
-						<Protected>
-							<NavigationBar />
-							<HomePage />
-						</Protected>
-					}
-				/>
-			</Routes>
-		</Router>
-		<ToastContainer />
-	</>
+    <>
+        <Router>
+            <Routes>
+                <Route path='/' element={<Navigate to='/app/home' />} />
+                <Route
+                    path='/auth/signup'
+                    element={
+                        <Gateway>
+                            <SignupPage />
+                        </Gateway>
+                    }
+                />
+                <Route
+                    path='/auth/login'
+                    element={
+                        <Gateway>
+                            <LoginPage />
+                        </Gateway>
+                    }
+                />
+                <Route
+                    path='/app/home'
+                    element={
+                        // <Protected>
+                        // 	<NavigationBar />
+                        // 	<HomePage />
+                        // </Protected>
+                        <HomePage />
+                    }
+                />
+            </Routes>
+        </Router>
+        <ToastContainer />
+    </>
 );
 
 export default App;
--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -6,21 +6,51 @@ import styles from 'components/Dropdown.module.scss';
 
 const Dropdown = (props) => {
     const { placeholder, options, width, className } = props;
+    const [dropdownActive, setDropdownActive] = useState(false);
     const [selectedOption, setSelectedOption] = useState(null);
 
+    console.log('🚀 ~ file: Dropdown.jsx ~ line 10 ~ Dropdown ~ dropdownActive', dropdownActive);
+    console.log('🚀 ~ file: Dropdown.jsx ~ line 11 ~ Dropdown ~ selectedOption', selectedOption);
+
+    const getOptionsList = () => {
+        return (
+            <div className={styles.dropdownOptionsContainer} tabIndex='0'>
+                {options.map((option) => (
+                    <div
+                        className={classnames(styles.dropdownOption, selectedOption === option && styles.selected)}
+                        onClick={() => setSelectedOption(option)}
+                        key={option}
+                    >
+                        <h3 className={styles.dropdownOptionText}>{option}</h3>
+                        {selectedOption === option && (
+                            <Icon name='IoCheckmark' size={20} color='var(--color-green-600)' className={styles.dropdownSelectedIcon} />
+                        )}
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const handleBlur = (event) => {
+        if (!event.currentTarget.contains(event.relatedTarget)) {
+            setDropdownActive(false);
+        }
+    };
+
     return (
-        <>
-            <button className={classnames(styles.dropdownButtonContainer, className)} style={{ width }}>
-                <h3 className={styles.dropdownTextPlaceholder}>{placeholder}</h3>
+        <div className={classnames(styles.dropdownContainer, className)} style={{ width }} onBlur={handleBlur}>
+            <button className={classnames(styles.dropdownButtonContainer, className)} onClick={() => setDropdownActive(true)}>
+                <h3 className={styles.dropdownTextPlaceholder}>{selectedOption || placeholder}</h3>
                 <Icon name='IoChevronDown' color='var(--color-gray-600)' size={22} className={styles.dropdownChevronIcon} />
             </button>
-        </>
+            {dropdownActive && getOptionsList()}
+        </div>
     );
 };
 
 Dropdown.propTypes = {
     placeholder: PropTypes.string,
-    options: PropTypes.object,
+    options: PropTypes.array,
     width: PropTypes.number,
 };
 
