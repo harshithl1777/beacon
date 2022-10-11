9d01d6da1e93b9893369ef4d84fc89079f27a72d
Harshith Latchupatula <harshithl1777@gmail.com>
2022-10-10 22:12:00 -0400

    Update useStoreID.js

--- client/src/services/hooks/useStoreID.js
+++ client/src/services/hooks/useStoreID.js
@@ -1,3 +1,5 @@
+import md5 from "md5";
+
 const useStoreID = (coordinates) => {
     const formattedCoordinates = `${coordinates.longitude}, ${coordinates.latitude}`;
     const storeID = md5(formattedCoordinates);
